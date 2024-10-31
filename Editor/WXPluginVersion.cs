36c0d299ea5c358b2666722e7563af7893408794
rainhong <rainhong@tencent.com>
2024-10-31 14:27:38 +0800

    Auto-publish.

--- Editor/WXPluginVersion.cs
+++ Editor/WXPluginVersion.cs
@@ -2,7 +2,7 @@ namespace WeChatWASM
 {
     public class WXPluginVersion
     {
-        public static string pluginVersion = "202410250329"; // 这一行不要改他，导出的时候会自动替换
+        public static string pluginVersion = "202410310627"; // 这一行不要改他，导出的时候会自动替换
     }
 
     public class WXPluginConf
