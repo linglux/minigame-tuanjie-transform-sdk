bac8428254dae3ad9580a70f1de6ba6218a06863
rainhong <rainhong@tencent.com>
2024-12-18 15:02:13 +0800

    Auto-publish.

--- Editor/WXConvertCore.cs
+++ Editor/WXConvertCore.cs
@@ -119,6 +119,7 @@ namespace WeChatWASM
             CheckBuildTarget();
             Init();
             ProcessWxPerfBinaries();
+            MakeEnvForLuaAdaptor();
             // JSLib
             SettingWXTextureMinJSLib();
             UpdateGraphicAPI();
@@ -216,6 +217,56 @@ namespace WeChatWASM
             return WXExportError.SUCCEED;
         }
 
+        private static int GetEnabledFlagStringIndex(string inAllText, string inTagStr)
+        {
+            try
+            {
+                int tagStrIdx = inAllText.IndexOf(inTagStr);
+                if (tagStrIdx == -1) throw new Exception($"Tag string '{inTagStr}' not found.");
+
+                int enabledStrIdx = inAllText.IndexOf("enabled: ", tagStrIdx);
+                if (enabledStrIdx == -1) throw new Exception("'enabled: ' string not found after tag.");
+
+                // inAllText[enabledStrIdx] == 'e'
+                // And that is to say, inAllText[enabledStrIdx + 9] should be 0 or 1
+                return enabledStrIdx + 9;
+            }
+            catch (Exception ex)
+            {
+                UnityEngine.Debug.LogError($"Failed to get enabled flag string index: {ex.Message}");
+                throw;
+            }
+        }
+
+        private static void SetPluginCompatibilityByModifyingMetadataFile(string inAssetPath, bool inEnabled)
+        {
+            try
+            {
+                string metaPath = AssetDatabase.GetTextMetaFilePathFromAssetPath(inAssetPath); // 获取.meta文件的路径
+                string enableFlagStr = inEnabled ? "1" : "0";
+
+                // 读取.meta文件
+                // 处理WebGL
+                string metaContent = File.ReadAllText(metaPath);
+                int idxWebGLEnableFlag = GetEnabledFlagStringIndex(metaContent, "WebGL: WebGL");
+
+                metaContent = metaContent.Remove(idxWebGLEnableFlag, 1).Insert(idxWebGLEnableFlag, enableFlagStr);
+                // WeixinMiniGame
+                int idxWeixinMiniGameEnableFlag = GetEnabledFlagStringIndex(metaContent, "WeixinMiniGame: WeixinMiniGame");
+
+                metaContent = metaContent.Remove(idxWeixinMiniGameEnableFlag, 1).Insert(idxWeixinMiniGameEnableFlag, enableFlagStr);
+
+                // 写回.meta文件
+
+                File.WriteAllText(metaPath, metaContent);
+                AssetDatabase.ImportAsset(inAssetPath, ImportAssetOptions.ForceUpdate);
+            }
+            catch (Exception ex)
+            {
+                UnityEngine.Debug.LogError($"Failed to enable plugin asset: {ex.Message}");
+            }
+        }
+
         private static void ProcessWxPerfBinaries()
         {
             string[] wxPerfPlugins;
@@ -250,6 +301,7 @@ namespace WeChatWASM
 #else
                 wxPerfJSBridgeImporter.SetCompatibleWithPlatform(BuildTarget.WebGL, config.CompileOptions.enablePerfAnalysis);
 #endif
+                SetPluginCompatibilityByModifyingMetadataFile(wxPerfPlugins[0], config.CompileOptions.enablePerfAnalysis);
             }
 
             {
@@ -262,11 +314,12 @@ namespace WeChatWASM
 #else
                 wxPerf2022Importer.SetCompatibleWithPlatform(BuildTarget.WebGL, bShouldEnablePerf2022Plugin);
 #endif
+                SetPluginCompatibilityByModifyingMetadataFile(wxPerfPlugins[1], bShouldEnablePerf2022Plugin);
             }
 
             {
                 // wx_perf_2021.a
-                bool bShouldEnablePerf2021Plugin = config.CompileOptions.enablePerfAnalysis && IsCompatibleWithUnity202103To202203();
+                bool bShouldEnablePerf2021Plugin = config.CompileOptions.enablePerfAnalysis && IsCompatibleWithUnity202102To202203();
 
                 var wxPerf2021Importer = AssetImporter.GetAtPath(wxPerfPlugins[2]) as PluginImporter;
 #if PLATFORM_WEIXINMINIGAME
@@ -274,36 +327,151 @@ namespace WeChatWASM
 #else
                 wxPerf2021Importer.SetCompatibleWithPlatform(BuildTarget.WebGL, bShouldEnablePerf2021Plugin);
 #endif
+                SetPluginCompatibilityByModifyingMetadataFile(wxPerfPlugins[2], bShouldEnablePerf2021Plugin);
+            }
+            AssetDatabase.Refresh();
+        }
+
+        /**
+         * Lua Adaptor Settings.
+         */
+
+        private static bool GetRequiredLuaHeaderFiles(out Dictionary<string, string> luaPaths)
+        {
+            luaPaths = new Dictionary<string, string>()
+            {
+                {"lua.h", null},
+                {"lobject.h", null},
+                {"lstate.h", null},
+                {"lfunc.h", null},
+                {"lapi.h", null},
+                {"lstring.h", null},
+                {"ltable.h", null},
+                {"lauxlib.h", null},
+            };
+
+            string rootPath = Directory.GetParent(Application.dataPath).ToString();
+            string[] paths = Directory.GetFiles(rootPath, "*.h", SearchOption.AllDirectories);
+            foreach (var path in paths)
+            {
+                string filename = Path.GetFileName(path);
+                if (luaPaths.ContainsKey(Path.GetFileName(path)))
+                {
+                    luaPaths[filename] = path;
+                }
             }
 
-            for (int i = 0; i < wxPerfPlugins.Length; i++)
+            foreach (var expectFile in luaPaths)
             {
-                var importer = AssetImporter.GetAtPath(wxPerfPlugins[i]) as PluginImporter;
-                importer.SaveAndReimport();
-                AssetDatabase.WriteImportSettingsIfDirty(wxPerfPlugins[i]);
-                AssetDatabase.Refresh();
+                if (expectFile.Value == null)
+                {
+                    return false;
+                }
             }
+
+            return true;
+        }
+
+        private static string GetLuaAdaptorPath(string filename)
+        {
+            string DS = WXAssetsTextTools.DS;
+            if (UnityUtil.GetSDKMode() == UnityUtil.SDKMode.Package)
+            {
+                return $"Packages{DS}com.qq.weixin.minigame{DS}Runtime{DS}Plugins{DS}LuaAdaptor{DS}{filename}";
+            }
+
+            return $"Assets{DS}WX-WASM-SDK-V2{DS}Runtime{DS}Plugins{DS}LuaAdaptor{DS}{filename}";
+        }
+
+        private static void MakeLuaImport(Dictionary<string, string> luaPaths)
+        {
+            string luaAdaptorImportHeaderPath = GetLuaAdaptorPath("lua_adaptor_import.h");
+            if (!File.Exists(luaAdaptorImportHeaderPath))
+            {
+                Debug.LogError("Lua Adaptor File Not Found: " + luaAdaptorImportHeaderPath);
+                return;
+            }
+
+            string includeLuaContent = "//EMSCRIPTEN_ENV_LUA_IMPORT_LOGIC_START";
+            foreach (var luaPath in luaPaths)
+            {
+                includeLuaContent += $"\n#include \"{luaPath.Value.Replace("\\", "\\\\")}\"";
+            }
+            includeLuaContent += "\n//EMSCRIPTEN_ENV_LUA_IMPORT_LOGIC_END";
+
+            string importHeaderContent = File.ReadAllText(luaAdaptorImportHeaderPath);
+            importHeaderContent = Regex.Replace(
+                importHeaderContent,
+                "//EMSCRIPTEN_ENV_LUA_IMPORT_LOGIC_START([\\s\\S]*?)//EMSCRIPTEN_ENV_LUA_IMPORT_LOGIC_END",
+                includeLuaContent
+            );
+
+            File.WriteAllText(luaAdaptorImportHeaderPath, importHeaderContent);
+        }
+
+        private static void ManageLuaAdaptorBuildOptions(bool shouldBuild)
+        {
+            string[] maybeBuildFiles = new string[]
+            {
+                "lua_adaptor_501.c",
+                "lua_adaptor_503.c",
+                "lua_adaptor_comm.c",
+                "lua_adaptor_import.h",
+            };
+
+            foreach (var maybeBuildFile in maybeBuildFiles)
+            {
+                string path = GetLuaAdaptorPath(maybeBuildFile);
+                if (!File.Exists(path) && shouldBuild)
+                {
+                    Debug.LogError("Lua Adaptor File Not Found: " + maybeBuildFile);
+                    continue;
+                }
+
+                var wxPerfJSBridgeImporter = AssetImporter.GetAtPath(path) as PluginImporter;
+                if (wxPerfJSBridgeImporter == null)
+                {
+                    Debug.LogError("Lua Adaptor Importer Not Found: " + maybeBuildFile);
+                    continue;
+                }
+#if PLATFORM_WEIXINMINIGAME
+                wxPerfJSBridgeImporter.SetCompatibleWithPlatform(BuildTarget.WeixinMiniGame, shouldBuild);
+#else
+                wxPerfJSBridgeImporter.SetCompatibleWithPlatform(BuildTarget.WebGL, shouldBuild);
+#endif
+                SetPluginCompatibilityByModifyingMetadataFile(path, shouldBuild);
+            }
+        }
+
+        private static void MakeEnvForLuaAdaptor()
+        {
+            bool hasLuaEnv = GetRequiredLuaHeaderFiles(out var luaPaths);
+            if (hasLuaEnv)
+            {
+                MakeLuaImport(luaPaths);
+            }
+
+            ManageLuaAdaptorBuildOptions(hasLuaEnv && config.CompileOptions.enablePerfAnalysis);
         }
 
         private static bool IsCompatibleWithUnity202203OrNewer()
         {
 #if UNITY_2022_3_OR_NEWER
             return true;
-#endif
+#else
             return false;
+#endif
         }
 
-        static bool IsCompatibleWithUnity202103To202203()
+        static bool IsCompatibleWithUnity202102To202203()
         {
 #if UNITY_2022_3_OR_NEWER
             return false;
-#endif
-
-#if !UNITY_2021_3_OR_NEWER
+#elif !UNITY_2021_2_OR_NEWER
             return false;
-#endif
-
+#else
             return true;
+#endif
         }
 
         private static void CheckBuildTarget()
@@ -597,7 +765,7 @@ namespace WeChatWASM
 
             var header = "var OriginalAudioContext = window.AudioContext || window.webkitAudioContext;window.AudioContext = function() {if (this instanceof window.AudioContext) {return wx.createWebAudioContext();} else {return new OriginalAudioContext();}};";
 
-            if (config.CompileOptions.DevelopBuild)
+            if (config.CompileOptions.DevelopBuild && config.CompileOptions.enablePerfAnalysis)
             {
                 header = header + RenderAnalysisRules.header;
                 for (i = 0; i < RenderAnalysisRules.rules.Length; i++)
@@ -642,10 +810,11 @@ namespace WeChatWASM
             PlayerSettings.WebGL.emscriptenArgs = string.Empty;
             if (WXExtEnvDef.GETDEF("UNITY_2021_2_OR_NEWER"))
             {
-                PlayerSettings.WebGL.emscriptenArgs += " -s EXPORTED_FUNCTIONS=_sbrk,_emscripten_stack_get_base,_emscripten_stack_get_end -s ERROR_ON_UNDEFINED_SYMBOLS=0";
+                PlayerSettings.WebGL.emscriptenArgs += " -s EXPORTED_FUNCTIONS=_sbrk,_emscripten_stack_get_base,_emscripten_stack_get_end";
 #if UNITY_2021_2_5
-                    PlayerSettings.WebGL.emscriptenArgs += ",_main";
+                PlayerSettings.WebGL.emscriptenArgs += ",_main";
 #endif
+                PlayerSettings.WebGL.emscriptenArgs += " -s ERROR_ON_UNDEFINED_SYMBOLS=0";
             }
 #endif
             PlayerSettings.runInBackground = false;
@@ -840,7 +1009,7 @@ namespace WeChatWASM
             }
         }
         /// <summary>
-        /// 等brotli之后，统计下资源包加brotli压缩后代码包是否超过了20M（小游戏代码分包总大小限制）
+        /// 等brotli之后，统计下资源包加brotli压缩后代码包是否超过了30M（小游戏代码分包总大小限制）
         /// </summary>
         private static void convertDataPackage(bool brotliError)
         {
@@ -911,8 +1080,8 @@ namespace WeChatWASM
                 // 计算首资源包大小
                 var tempDataInfo = new FileInfo(tempDataPath);
                 var tempFileSize = tempDataInfo.Length.ToString();
-                // 胶水层及sdk可能占一定大小，粗略按照1M来算，则剩余19M
-                if (brcodeSize + int.Parse(tempFileSize) > (20 - 1) * 1024 * 1024)
+                // 胶水层及sdk可能占一定大小，粗略按照1M来算，则剩余29M
+                if (brcodeSize + int.Parse(tempFileSize) > (30 - 1) * 1024 * 1024)
                 {
                     config.ProjectConf.assetLoadType = 0;
                     Debug.LogError("资源文件过大，不适宜用放小游戏包内加载，请上传资源文件到CDN");
